e2b8c995c0e96367f6575818e24b88c322feba7a
Jacob Cazacov <jacob.cazacov@gmail.com>
2021-12-01 23:24:39 +0100

    day 01

---
+++ AdventOfCode2021/day01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+namespace day01
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] allLines = File.ReadAllLines("Input.txt");
+            
+            int previousSum = Int32.MaxValue;
+            int biggerNumbers = 0;
+
+            for (int i = 2; i < allLines.Length; i++)
+            {
+                int currentSum = Int32.Parse(allLines[i]) + Int32.Parse(allLines[i - 1]) + Int32.Parse(allLines[i - 2]);
+                if (currentSum > previousSum)
+                {
+                    biggerNumbers++;
+                }
+
+                previousSum = currentSum;
+            }
+            Console.WriteLine(biggerNumbers);
+        }
+    }
+}
