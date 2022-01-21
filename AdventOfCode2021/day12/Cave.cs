7a624be39dabe3bd3648d1b3d2cd3c670ca6cc4d
Jacob Cazacov <jacob.cazacov@gmail.com>
2022-01-21 22:01:15 +0100

    day 12

---
+++ AdventOfCode2021/day12/Cave.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace day12.Properties
+{
+    public class Cave
+    {
+        private readonly List<Cave> neighbours;
+        private readonly string name;
+        private readonly bool smallCave;
+        private int visits;
+        private bool twice;
+
+        public Cave(string name)
+        {
+            neighbours = new List<Cave>();
+            this.name = name;
+            visits = 0;
+            twice = false;
+            
+            smallCave = Char.ToLower(name[0]) == name[0];
+            
+        }
+        
+        public int findAllPaths(bool twiceChosen, bool twiceUsed)
+        {
+            if (name == "end" && !(twiceChosen && !twiceUsed))
+            {
+                return 1;
+            }
+            else if (name == "end")
+            {
+                return 0;
+            }
+
+            if (smallCave && (twice && visits > 1 || !twice && visits > 0))
+                return 0;
+
+            visits++;
+            
+            int allPaths = 0;
+            
+            
+            if (!twiceChosen && name != "start" && smallCave)
+            {
+                twice = true;
+                foreach (var neighbour in neighbours)
+                {
+                    if (neighbour.getName() != "start")
+                    {
+                        allPaths += neighbour.findAllPaths(true, twiceUsed);
+                    }
+                }
+                twice = false;
+            }
+
+            if (!twiceUsed && twice)
+            {
+                twiceUsed = true;
+                foreach (var neighbour in neighbours)
+                {
+                    if (neighbour.getName() != "start")
+                    {
+                        allPaths += neighbour.findAllPaths(twiceChosen, twiceUsed);
+                    }
+                }
+                twiceUsed = false;
+            }
+            else
+            {
+                foreach (var neighbour in neighbours)
+                {
+                    if (neighbour.getName() != "start")
+                    {
+                        allPaths += neighbour.findAllPaths(twiceChosen, twiceUsed);
+                    }
+                }
+            }
+            
+            visits--;
+            
+            return allPaths;
+        }
+
+        public void addNeighbour(Cave neighbour)
+        {
+            if (!neighbours.Contains(neighbour))
+            {
+                neighbours.Add(neighbour);
+            }
+        }
+
+        public List<Cave> getNeighbours()
+        {
+            return neighbours;
+        }
+
+        public bool isBigCave()
+        {
+            return smallCave;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public override string ToString()
+        {
+            string result = name + " (";
+            for (int i = 0; i < neighbours.Count; i++)
+            {
+                result += neighbours[i].getName();
+                if (i < neighbours.Count - 1)
+                    result += ",";
+            }
+
+            result += ")";
+            return result;
+        }
+    }
+}
--- AdventOfCode2021/day12/Program.cs
+++ AdventOfCode2021/day12/Program.cs
@@ -1,9 +1,54 @@
-﻿namespace day12
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using day12.Properties;
+
+namespace day12
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            string[] allLines = File.ReadAllLines("Input2.txt");
+            string pattern = @"(\w+)-(\w+)";
+            Dictionary<string, Cave> allCaves = new Dictionary<string, Cave>();
+
+            foreach (var line in allLines)
+            {
+                var match = Regex.Match(line, pattern);
+                
+                string cave1 = match.Groups[1].Value;
+                string cave2 = match.Groups[2].Value;
+
+                if (!allCaves.ContainsKey(cave1))
+                {
+                    allCaves.Add(cave1, new Cave(cave1));
+                }
+                if (!allCaves.ContainsKey(cave2))
+                {
+                    allCaves.Add(cave2, new Cave(cave2));
+                }
+                
+                allCaves[cave1].addNeighbour(allCaves[cave2]);
+                allCaves[cave2].addNeighbour(allCaves[cave1]);
+            }
+
+            printCaves(allCaves);
+
+            //solution for part 2
+            //If both parameters of this findAllPaths-function are set to true, it will print the solution for part 1
+            Console.WriteLine("There are " + allCaves["start"].findAllPaths(false, false) + " distinct paths.");
+            
+        }
+
+        public static void printCaves(Dictionary<string, Cave> allCaves)
+        {
+            foreach (var cave in allCaves)
+            {
+                Console.WriteLine(cave.Value.ToString());
+            }
         }
     }
 }
